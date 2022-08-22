44273fe293d81bebba2d3fa647ba1cd9097352c1
Casey <orangobang@gmail.com>
2022-08-22 18:25:52 -0400

    eventlisteners

--- src/modules/createNav.js
+++ src/modules/createNav.js
@@ -13,23 +13,21 @@ export function createNav() {
     header.innerHTML = `<h1 id='headerTitle'>TODO List</h1>
     <input id='searchbar' type='text' placeholder='Search'>`
     sidebar.innerHTML = `
-    <h1>Assignments</h1>
-    <button>Today</button>
-    <button>This week</button>
-    <button>Calendar</button>
-    <h1>My Projects</h1>
-    <button>Odin</button>
-    <button>Add Project</h1>`
+    <h1 id='assignmentTitle'>Assignments</h1>
+    <h1 id='projectTitle'>My Projects</h1>
+    `
 
     const sideButtonToday = ButtonFactory('todayBtn', 'sideBtns', 'Today')
     const sideButtonThisWeek = ButtonFactory('thisWeekBtn', 'sideBtns', 'This Week')
     const sideButtonCalendar = ButtonFactory('calendar', 'sideBtns', 'Calendar')
     const sideButtonOdin = ButtonFactory('project', 'sideBtns', 'Odin');
+    const sideButtonAdd = ButtonFactory('addProject', 'sideBtn', 'Add Project');
 
-    sidebar.appendChild(sideButtonToday)
+    sidebar.appendChild(sideButtonToday).after()
     sidebar.appendChild(sideButtonThisWeek)
     sidebar.appendChild(sideButtonCalendar)
     sidebar.appendChild(sideButtonOdin)
+    sidebar.appendChild(sideButtonAdd)
 
 
     page.appendChild(header);
