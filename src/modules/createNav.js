76e8b0dcc670fe5f54bcffa5349574179a2c3e0f
Casey <orangobang@gmail.com>
2022-08-22 21:22:50 -0400

    corrected button layout on side bar

--- src/modules/createNav.js
+++ src/modules/createNav.js
@@ -13,26 +13,25 @@ export function createNav() {
     header.innerHTML = `<h1 id='headerTitle'>TODO List</h1>
     <input id='searchbar' type='text' placeholder='Search'>`
     sidebar.innerHTML = `
-    <h1 id='assignmentTitle'>Assignments</h1>
-    <h1 id='projectTitle'>My Projects</h1>
-    `
+    <h1 id='assignmentTitle'>Assignments</h1>`
 
     const sideButtonToday = ButtonFactory('todayBtn', 'sideBtns', 'Today')
     const sideButtonThisWeek = ButtonFactory('thisWeekBtn', 'sideBtns', 'This Week')
     const sideButtonCalendar = ButtonFactory('calendar', 'sideBtns', 'Calendar')
     const sideButtonOdin = ButtonFactory('project', 'sideBtns', 'Odin');
     const sideButtonAdd = ButtonFactory('addProject', 'sideBtn', 'Add Project');
-
-    sidebar.appendChild(sideButtonToday).after()
+    
+    sidebar.appendChild(sideButtonToday)
     sidebar.appendChild(sideButtonThisWeek)
     sidebar.appendChild(sideButtonCalendar)
+
+    sidebar.innerHTML +=`<h1 id='projectTitle'>My Projects</h1>`
     sidebar.appendChild(sideButtonOdin)
     sidebar.appendChild(sideButtonAdd)
 
-
     page.appendChild(header);
     page.appendChild(sidebar);
-    page.appendChild(footer);    
+    page.appendChild(footer); 
 }
 
 function ButtonFactory(id, classname, text) { 
