52741552047b758fef62bec4accee7944134b288
Casey <orangobang@gmail.com>
2022-08-23 16:19:42 -0400

    add task btn working / style form next

--- src/modules/tasks.js
+++ src/modules/tasks.js
@@ -3,14 +3,15 @@ export function createTask() {
 
     const taskFormBox = document.createElement('div');
     taskFormBox.innerHTML =`<form id='taskForm'>
+    <h1> Task info</h1>
     <input id='newTaskDescription' placeholder='Description'>
     <h2>Task Prioity</h2>
     Low<input type=radio id='newTaskPrioLow' name='newTaskPrio'>
     Medium<input type=radio id='newTaskPrioMedium' name='newTaskPrio'>
     High<input type=radio id='newTaskPrioHigh' name='newTaskPrio'>
     <h2>Completed</h2>
-    True<input id='newTaskCompletedTrue' name='newTaskCompleted'>
-    False<input id='newTaskCompletedFalse'name='newTaskCompleted'>
+    True<input type='radio' id='newTaskCompletedTrue' name='newTaskCompleted'>
+    False<input type='radio' id='newTaskCompletedFalse'name='newTaskCompleted'>
     <button onlcick='addNewTask()' id='addNewTaskBtn'>Add Task</button>
     </form>`
 
--- src/modules/today.js
+++ src/modules/today.js
@@ -2,12 +2,18 @@ import { createTask } from "./tasks";
 
 export function loadTodayTasks() {
     const page = document.getElementById('content');
-
+    const addBtn = document.createElement('button');
+    addBtn.id = 'addTaskBtn';
+    addBtn.textContent = 'Add New Task';
+    addBtn.onclick = function() {
+        createTask()
+    }
     const todayPage = document.createElement('div');
     todayPage.id = 'todayPage';
     todayPage.className = 'pageContent';
 
-    todayPage.innerHTML = `<h1> Todays Tasks</h1>
-    <button onclick=createTask()' id='addTaskBtn'>Add New Task</button>`
+
+    todayPage.innerHTML = `<h1> Todays Tasks</h1>`
+    todayPage.appendChild(addBtn)
     page.appendChild(todayPage);
 }
