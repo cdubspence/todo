006173b91321d3990cce31c3937f796a7f9327ea
Casey <orangobang@gmail.com>
2022-08-24 14:58:04 -0400

    task form created and styled

--- src/modules/tasks.js
+++ src/modules/tasks.js
@@ -2,20 +2,27 @@ export function createTask() {
     const page = document.getElementById('content');
 
     const taskFormBox = document.createElement('div');
+    taskFormBox.id = 'taskFormBox';
     taskFormBox.innerHTML =`<form id='taskForm'>
     <h1> Task info</h1>
     <input id='newTaskDescription' placeholder='Description'>
     <h2>Task Prioity</h2>
-    Low<input type=radio id='newTaskPrioLow' name='newTaskPrio'>
-    Medium<input type=radio id='newTaskPrioMedium' name='newTaskPrio'>
-    High<input type=radio id='newTaskPrioHigh' name='newTaskPrio'>
+    <label for='newTaskPrioLow'><input type=radio id='newTaskPrioLow' name='newTaskPrio'>Low</label>
+    
+    <label for='newTaskPrioMedium'><input type=radio id='newTaskPrioMedium' name='newTaskPrio'>Medium</label>
+    <label for='newTaskPrioHigh'><input type=radio id='newTaskPrioHigh' name='newTaskPrio'>High</label>
+
     <h2>Completed</h2>
     True<input type='radio' id='newTaskCompletedTrue' name='newTaskCompleted'>
     False<input type='radio' id='newTaskCompletedFalse'name='newTaskCompleted'>
     <button onlcick='addNewTask()' id='addNewTaskBtn'>Add Task</button>
+    <button id='formCloseBtn'>X</button>
     </form>`
 
-    page.appendChild(taskFormBox);
+    page.style.backgroundColor = '#495464'
+    page.style.opacity = '.5'
+    document.body.appendChild(taskFormBox)
+    taskFormBox.style.display = 'block'
 }
 
 function addNewTask() {
--- src/modules/today.js
+++ src/modules/today.js
@@ -3,6 +3,8 @@ import { createTask } from "./tasks";
 export function loadTodayTasks() {
     const page = document.getElementById('content');
     const addBtn = document.createElement('button');
+    const taskArea = document.createElement('div');
+    taskArea.id = 'todaysTasks'
     addBtn.id = 'addTaskBtn';
     addBtn.textContent = 'Add New Task';
     addBtn.onclick = function() {
@@ -14,6 +16,7 @@ export function loadTodayTasks() {
 
 
     todayPage.innerHTML = `<h1> Todays Tasks</h1>`
+    todayPage.appendChild(taskArea)
     todayPage.appendChild(addBtn)
     page.appendChild(todayPage);
 }
